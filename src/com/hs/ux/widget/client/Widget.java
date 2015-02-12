5ad19e99f35c392b8db475ab52135d985cddbbd1
limingjian1023@gmail.com <limingjian1023@gmail.com>
2015-02-12 18:02:30 +0100

    refactoring

--- src/com/hs/ux/widget/client/Widget.java
+++ src/com/hs/ux/widget/client/Widget.java
@@ -2,6 +2,7 @@ package com.hs.ux.widget.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.hs.ux.widget.client.ui.MainContainer;
 
 public class Widget implements EntryPoint {
 
